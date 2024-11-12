c1845f34450258b5f745b0aeeff4533bc1cc46eb
Emily McLean <emilymcleandeveloper@gmail.com>
2024-11-12 13:40:06 +1100

    Handle arguments better (i.e. support named arguments)

--- templates/entrypoint.template.sh
+++ templates/entrypoint.template.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-formatted_args=("${@:1:$#-1}")
-formatted_args+=("-o" "${!#}")
+pos_args=()
 
-mkdir -p "${!#}"
-{{ .Env.call }} "${formatted_args[@]}"
+while [[ $# -gt 0 ]] && [[ "$1" != -* ]]; do
+  pos_args+=("$1")
+  shift
+done
+
+while getopts ":o:" opt; do
+  case "${opt}" in
+    o)
+        output_folder="$OPTARG"
+        ;;
+  esac
+done
+
+echo "Positional arguments before options: ${pos_args[@]}"
+echo "Remaining arguments after options: $@"
+
+mkdir -p "$output_folder"
+{{ .Env.call }} "${pos_args[@]}" $@
